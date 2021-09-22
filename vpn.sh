8b9b23b7f6e81e8804f7fe9b0c05adbbab63340e
Cong Minh Vuong <cminh.v566@gmail.com>
2021-09-22 16:54:27 +0700

    Add files via upload

--- vpn.sh
+++ vpn.sh
@@ -1,4 +1,29 @@
-# Check OS version
+#!/bin/bash
+
+RED='\033[0;31m'
+ORANGE='\033[0;33m'
+NC='\033[0m'
+
+function isRoot() {
+	if [ "${EUID}" -ne 0 ]; then
+		echo "You need to run this script as root"
+		exit 1
+	fi
+}
+
+function checkVirt() {
+	if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "OpenVZ is not supported"
+		exit 1
+	fi
+
+	if [ "$(systemd-detect-virt)" == "lxc" ]; then
+		echo "LXC is not supported"
+	fi
+}
+
+function checkOS() {
+	# Check OS version
 	if [[ -e /etc/debian_version ]]; then
 		source /etc/os-release
 		OS="${ID}" # debian or ubuntu
@@ -20,8 +45,15 @@
 		echo "Looks like you aren't running this installer on a Debian, Ubuntu, Fedora, CentOS or Arch Linux system"
 		exit 1
 	fi
+}
 
-# Install Wget to Download Script
+function initialCheck() {
+	isRoot
+	checkVirt
+	checkOS
+}
+
+function wgetinstall() {
 	if [[ ${OS} == 'ubuntu' ]] || [[ ${OS} == 'debian' && ${VERSION_ID} -gt 10 ]]; then
 		apt-get update
 		apt-get install -y wget
@@ -35,6 +67,12 @@
 	elif [[ ${OS} == 'arch' ]]; then
 		pacman -S --needed --noconfirm wget
 	fi
+}
+
+# Check for root, virt, OS...
+initialCheck
+wgetinstall
+
 # Script Install
 wget https://raw.githubusercontent.com/minhvip20956/wireguard-install/main/cmvpn -O /usr/bin/cmvpn
 chmod +x /usr/bin/cmvpn
