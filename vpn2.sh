e6b7950f074da3b2959cb29b9a10acb8a13063f9
Cong Minh Vuong <cminh.v566@gmail.com>
2021-09-22 22:04:25 +0700

    Add files via upload

---
+++ vpn2.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+ORANGE='\033[0;33m'
+NC='\033[0m'
+
+function isRoot() {
+	if [ "${EUID}" -ne 0 ]; then
+		echo "You need to run this script as root"
+		exit 1
+	fi
+}
+
+function checkVirt() {
+	if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "OpenVZ is not supported"
+		exit 1
+	fi
+
+	if [ "$(systemd-detect-virt)" == "lxc" ]; then
+		echo "LXC is not supported"
+	fi
+}
+
+function checkOS() {
+	# Check OS version
+	if [[ -e /etc/debian_version ]]; then
+		source /etc/os-release
+		OS="${ID}" # debian or ubuntu
+		if [[ ${ID} == "debian" || ${ID} == "raspbian" ]]; then
+			if [[ ${VERSION_ID} -lt 10 ]]; then
+				echo "Your version of Debian (${VERSION_ID}) is not supported. Please use Debian 10 Buster or later"
+				exit 1
+			fi
+		fi
+	elif [[ -e /etc/fedora-release ]]; then
+		source /etc/os-release
+		OS="${ID}"
+	elif [[ -e /etc/centos-release ]]; then
+		source /etc/os-release
+		OS=centos
+	elif [[ -e /etc/arch-release ]]; then
+		OS=arch
+	else
+		echo "Looks like you aren't running this installer on a Debian, Ubuntu, Fedora, CentOS or Arch Linux system"
+		exit 1
+	fi
+}
+
+function initialCheck() {
+	isRoot
+	checkVirt
+	checkOS
+}
+
+function wgetinstall() {
+	if [[ ${OS} == 'ubuntu' ]] || [[ ${OS} == 'debian' && ${VERSION_ID} -gt 10 ]]; then
+		apt-get update
+		apt-get install -y wget
+	elif [[ ${OS} == 'debian' ]]; then
+		apt update
+		apt-get install -y wget
+	elif [[ ${OS} == 'fedora' ]]; then
+		dnf install -y wget
+	elif [[ ${OS} == 'centos' ]]; then
+		yum -y install wget
+	elif [[ ${OS} == 'arch' ]]; then
+		pacman -S --needed --noconfirm wget
+	fi
+}
+
+# Check for root, virt, OS...
+initialCheck
+wgetinstall
+
+# Script Install
+wget https://raw.githubusercontent.com/minhvip20956/wireguard-install/main/v2/cmvpn -O /usr/bin/cmvpn
+chmod +x /usr/bin/cmvpn
+cmvpn
