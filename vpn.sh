c6df0a0a3761948d24375c75bb32c582cff9014b
Cong Minh Vuong <cminh.v566@gmail.com>
2021-09-22 16:37:56 +0700

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -1,3 +1,18 @@
+# Install Wget to Download Script
+	if [[ ${OS} == 'ubuntu' ]] || [[ ${OS} == 'debian' && ${VERSION_ID} -gt 10 ]]; then
+		apt-get update
+		apt-get install -y wget
+	elif [[ ${OS} == 'debian' ]]; then
+		apt update
+		apt-get install -y wget
+	elif [[ ${OS} == 'fedora' ]]; then
+		dnf install -y wget
+	elif [[ ${OS} == 'centos' ]]; then
+		yum -y install wget
+	elif [[ ${OS} == 'arch' ]]; then
+		pacman -S --needed --noconfirm wget
+	fi
+# Script Install
 wget https://raw.githubusercontent.com/minhvip20956/wireguard-install/main/cmvpn -O /usr/bin/cmvpn
 chmod +x /usr/bin/cmvpn
 cmvpn
